efed865bf8eb78a92834f300ebd22329e64e0fab
DucViet519 <ducviet519@gmail.com>
2022-10-28 14:13:50 +0700

    ok

--- AppBVTA/Controllers/ReportsController.cs
+++ AppBVTA/Controllers/ReportsController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Rotativa.AspNetCore;
+using Rotativa.AspNetCore.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataBVTA.Services;
+using DataBVTA.Models.Entities;
 
 namespace AppBVTA.Controllers
 {
@@ -14,11 +17,24 @@ namespace AppBVTA.Controllers
             return View();
         }
 
+
+
         public IActionResult PhieuKhamBenh()
         {
-            return View();
-            //return new ViewAsPdf("PhieuKhamBenh");
+            ExportReport data = new ExportReport();
+            data.BarCode = StaticHelper.GenBarCode("12345678");
+            data.TenPhieuIn = "PhieuKhamBenh";
+
+            //return View(data);
+            return new ViewAsPdf("PhieuKhamBenh", data)
+            {
+                //FileName = $"{data.TenPhieuIn}.pdf", //User for click download
+                PageSize = Size.A4,
+                PageOrientation = Orientation.Portrait,
+                IsJavaScriptDisabled = true,
+            };
+
         }
-       
+
     }
 }
